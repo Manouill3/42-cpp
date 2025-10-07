e59288a677e370473694de830a7fbe921a04a4aa
Manouill3 <manuarii.degache@gmail.com>
2025-10-07 19:00:24 +0200

    cpp03

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdegache <mdegache@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 18:52:23 by mdegache          #+#    #+#             */
+/*   Updated: 2025/10/07 18:59:39 by mdegache         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap {
+  
+private :
+
+    std::string name;
+    
+};
+
+#endif
