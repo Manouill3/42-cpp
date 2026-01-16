8716644f2f4f752810339064943c3fbf6643a9d6
Manuarii Degache <mdegache@2C1.42angouleme.fr>
2026-01-16 12:41:51 +0100

    cpp05ex03

---
+++ cpp05/ex03/Intern.cpp
@@ -0,0 +1,13 @@
+#include "Intern.hpp"
+
+Intern::Intern() {}
+
+Intern::Intern(const Intern &obj) {}
+
+Intern &Intern::operator=(const Intern &obj) { return *this; }
+
+Intern::~Intern() {}
+
+AForm *Intern::makeForm(std::string formType, std::string target) {
+    
+}
---
+++ cpp05/ex03/Intern.hpp
@@ -0,0 +1,19 @@
+#ifndef INTERN_HPP
+#define INTERN_HPP
+
+#include <iostream>
+#include "AForm.hpp"
+
+class Intern {
+
+    public :
+
+        Intern();
+        Intern(const Intern &obj);
+        Intern &operator=(const Intern &obj);
+        ~Intern();
+
+        AForm *makeForm(std::string formType, std::string target);
+};
+
+#endif
