39d431515e64610c1ce5298a7a81159e18fe892c
Manouill3 <manuarii.degache@gmail.com>
2026-01-19 09:23:42 +0100

    finn cpp05

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -9,5 +9,26 @@ Intern &Intern::operator=(const Intern &obj) { return *this; }
 Intern::~Intern() {}
 
 AForm *Intern::makeForm(std::string formType, std::string target) {
+    int index;
+
+    std::string type[] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+    for (int i = 0; i < 4; i++) {
+        if (type[i] == formType)
+            index = i;
+    }
     
+    switch (index) {
+        case 0:
+            std::cout << "Intern created shrubbery creation form !" << std::endl;
+            return new ShrubberyCreationForm(target);
+        case 1:
+            std::cout << "Intern created robotomy request form !" << std::endl;
+            return new RobotomyRequestForm(target);
+        case 2:
+            std::cout << "Intern created presidential pardon form !" << std::endl;
+            return new PresidentialPardonForm(target);
+        default:
+            std::cout << "The provide form type does not exist ! Choose between shrubbery creation, robotomy request or presidential pardon." << std::endl;
+            break;
+    }
 }
