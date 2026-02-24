4d9c46ec5e84db44c3e2351e4e8172b396a3db5d
Manuarii Degache <mdegache@2F2.42angouleme.fr>
2026-02-24 15:47:34 +0100

    fin 06

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -2,6 +2,8 @@
 #define BASE_HPP
 
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 
 class Base {
 
--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -1,17 +1,60 @@
-#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 Base *generate(void) {
+    srand(time(0));
+    int randomNumber = rand() % 3;
     
+    switch (randomNumber) {
+        case 0 :
+            return new A();
+        case 1 :
+            return new B();
+        case 2 :
+            return  new C();
+        default :
+            return NULL;    
+    }
 }
 
 void identify(Base *p) {
-
+    if (dynamic_cast<A*>(p))
+        std::cout << "The type of p is A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "The type of p is B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "The type of p is C" << std::endl;
+    else
+        std::cout << "The type of p is unknown" << std::endl;
 }
 
 void identify(Base &p) {
-
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "The type of p is A" << std::endl;
+    }
+    catch (...) {
+        try {
+            (void)dynamic_cast<B&>(p);
+            std::cout << "The type of p is B" << std::endl;
+        }
+        catch (...) {
+            try {
+                (void)dynamic_cast<C&>(p);
+                std::cout << "The type of p is C" << std::endl;
+            }
+            catch (...) {
+                std::cout << "The type of p is unknown" << std::endl;
+            }
+        }
+    }
 }
 
 int main(void) {
+    Base *randomtype;
 
+    randomtype = generate();
+    identify(randomtype);
+    identify(*randomtype);
 }
