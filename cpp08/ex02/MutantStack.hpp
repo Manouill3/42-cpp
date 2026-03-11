abe80a03aa545f0d22cd0ca6ff92c46950be443b
Manuarii Degache <mdegache@2A7.42angouleme.fr>
2026-03-11 14:56:13 +0100

    debut 09

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,35 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <list>
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+
+    public :
+
+        MutantStack();
+        MutantStack(const MutantStack &obj);
+        MutantStack &operator=(const MutantStack &obj);
+        ~MutantStack();
+    
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin();
+        iterator end();
+
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        const_iterator begin() const;
+        const_iterator end() const;
+
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        reverse_iterator rbegin();
+        reverse_iterator rend();
+
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+        const_reverse_iterator rbegin() const;
+        const_reverse_iterator rend() const;
+};
+
+#endif
