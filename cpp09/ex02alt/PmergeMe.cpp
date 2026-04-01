5854f28bcd894e317977760f2a1800507bb0bc48
Manouill3 <manuarii.degache@gmail.com>
2026-04-01 21:35:03 +0200

    up

--- cpp09/ex02alt/PmergeMe.cpp
+++ cpp09/ex02alt/PmergeMe.cpp
@@ -42,4 +42,131 @@ PmergeMe<T> &PmergeMe<T>::operator=(const PmergeMe &obj) {
 template <typename T>
 PmergeMe<T>::~PmergeMe() {}
 
+template <typename T>
+void PmergeMe<T>::swap_size(size_t size, int idx1, int idx2) {
+    for (size_t i = 0; i < size / 2; i++) {
+        int swap;
 
+        swap = tab[idx1 - i];
+        tab[idx1 - i] = tab[idx2 - i];
+        tab[idx2 - i] = swap;
+    }
+}
+
+template <typename T>
+void PmergeMe<T>::sortPair() {
+    size_t pairSize = 2;
+
+    while (pairSize <= tab.size()) {
+        for (size_t i = pairSize; i <= tab.size(); i += pairSize) {
+            if (tab[i - 1] < tab[i - pairSize / 2 - 1])
+                swap_size(pairSize, i - 1, i - pairSize / 2 - 1);
+        }
+        pairSize *= 2;
+    }
+    merge(pairSize / 2);
+}
+
+template <typename T>
+void PmergeMe<T>::merge(size_t size) {
+    while (size > 0) {
+        jacob(size);
+        size /= 2;
+    }
+}
+
+template <typename T>
+void PmergeMe<T>::jacob(size_t size) {
+    setVec(size);
+
+    size_t count = 1;
+    while (!pend.empty()) {
+        int len = jacoblist[count] - jacoblist[count - 1];
+        while (len > 0) {
+            int idx = binary(pend[len * size - 1], size);
+            addPair(idx, len * size, size);
+            len--;
+        }
+        count++;
+        if (count + 1 > jacoblist.size())
+            jacoblist.push_back(jacoblist[count] + (jacoblist[count - 1] * 2));
+    }
+    for (size_t i = 0; i < rest.size(); i++)
+        result.push_back(rest[i]);
+    tab = result;
+
+    for (size_t i = 0; i < tab.size(); i++)
+        std::cout << tab[i] << " ";
+    std::cout << std::endl;
+}
+
+template <typename T>
+void PmergeMe<T>::setVec(size_t size) {
+    result.clear();
+    pend.clear();
+    rest.clear(); 
+
+    int r = tab.size() % size;
+    int count = 0;
+    for (size_t i = size; i <= tab.size() - r; i += size) {
+        size_t tmp = size;
+
+        if (i == size || i == size * 2) {
+            while (tmp > 0) {
+                result.push_back(tab[i - tmp]);
+                tmp--;
+            }
+        }
+        else if (count % 2 == 0) {
+            while (tmp > 0) {
+                pend.push_back(tab[i - tmp]);
+                tmp--;
+            }
+            count++;
+        }
+        else {
+            while (tmp > 0) {
+                result.push_back(tab[i - tmp]);
+                tmp--;
+            }
+            count++;
+        }
+    }
+    for (size_t i = tab.size() - r; i < tab.size(); i++)
+        rest.push_back(tab[i]);
+}
+
+template <typename T>
+int PmergeMe<T>::binary(int val, size_t size) {
+    T max;
+
+    for (size_t i = size; i <= result.size(); i += size)
+        max.push_back(result[i - 1]);
+
+    int beg = 0;
+    int end = max.size();
+    int m;
+    while (beg <= end) {
+        m = beg + (end - beg) / 2;
+        if (beg >= end)
+            break;
+        if (max[m] < val)
+            beg = m + 1;
+        else if (m > 0 && max[m - 1] > val)
+            end = m - 1;
+        else
+            break ;
+    }
+    
+    return m;
+}
+
+template <typename T>
+void PmergeMe<T>::addPair(int newIdx, int idxPair, size_t size) {
+    
+    for (size_t i = idxPair; i > idxPair - size; i--)
+        result.insert(result.begin() + newIdx * size, pend[i - 1]);
+
+    for (size_t i = idxPair; i > idxPair - size; i--)
+        pend.erase(pend.begin() + i - 1);
+}
--- cpp09/ex02alt/PmergeMe.hpp
+++ cpp09/ex02alt/PmergeMe.hpp
@@ -25,14 +25,14 @@ class PmergeMe {
         PmergeMe &operator=(const PmergeMe &obj);
         ~PmergeMe();
 
-        // void sortPair();
-        // void swap_size(size_t size, int idx1, int idx2);
+        void swap_size(size_t size, int idx1, int idx2);
+        void sortPair();
 
-        // void merge(size_t size);
-        // void jacob(size_t size);
-        // void setVec(size_t size);
-        // int binary(int val, size_t size);
-        // void addPair(int newIdx, int idxPair, size_t size);
+        void merge(size_t size);
+        void jacob(size_t size);
+        void setVec(size_t size);
+        int binary(int val, size_t size);
+        void addPair(int newIdx, int idxPair, size_t size);
 };
 
 #endif
